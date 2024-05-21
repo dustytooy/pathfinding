ca303fe266d2d8b1e7672daa01927fce4f5af3eb
dustytooy <dustytooy@gmail.com>
2024-05-22 00:29:11 +0900

    added init and reset for grid

--- Assets/Samples/Scripts/Entity/ITerrainGrid.cs
+++ Assets/Samples/Scripts/Entity/ITerrainGrid.cs
@@ -5,9 +5,11 @@
         public ITerrainCell[] cells { get; }
         public int width { get; }
         public int height { get; }
-
+        public void Initialize(GridConfiguration config);
+        public void Reset();
         public int ToIndex(int x, int y);
         public bool IsValidPosition(int x, int y);
+        public void SetCell(int x, int y, ITerrainCell cell);
         public ITerrainCell GetCell(int x, int y);
     }
 }
--- Assets/Samples/Scripts/Entity/Impl/TerrainGrid.cs
+++ Assets/Samples/Scripts/Entity/Impl/TerrainGrid.cs
@@ -1,12 +1,41 @@
 ﻿using Dustytoy.Collections;
+using Dustytoy.DI;
 
 namespace Dustytoy.Samples.Grid2D.Entity.Impl
 {
     internal class TerrainGrid : Grid2D<ITerrainCell>, ITerrainGrid
     {
         public ITerrainCell[] cells => array;
-        public TerrainGrid(int width, int height) : base(width, height)
+
+        public TerrainGrid() : base(0,0)
         {
+        }
+
+        [Inject]
+        public void Initialize(GridConfiguration config)
+        {
+            this.width = config.width;
+            this.height = config.height;
+            array = new ITerrainCell[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    SetCell(x, y, new TerrainCell(x, y, default, this));
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    GetCell(x, y).terrainProperties = default;
+                }
+            }
         }
     }
 }
